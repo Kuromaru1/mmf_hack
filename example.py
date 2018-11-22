9113cb2534ff69695b859df2afd5b9e0767576d9
rbkn99 <matveich1903@gmail.com>
2018-11-22 19:44:49 +0300

    upd!

--- example.py
+++ example.py
@@ -3,11 +3,13 @@ import pandas as pd
 from pppredictor import PPPredictor
 
 date = '2017-08-07'
+predict_date = '2017-11-06'
 
 prices_df = pd.read_csv('data/retrieved_data.csv', index_col='Date')
 phrases_df = pd.read_csv('data/proposed_phrases.csv')
 news_df = pd.read_csv('data/proposed_sites.csv', index_col='Date')
 
 ppp = PPPredictor()
-prepared_prices_df, prepared_phrases_df, prepared_news_df = ppp.prepare_data(data, date)
-ppp.fit((prepared_prices_df, prepared_phrases_df, prepared_news_df), use_text_model=False)
+prepared_prices_df, prepared_phrases_df, prepared_news_df = ppp.prepare_data((prices_df, phrases_df, news_df), date)
+ppp.fit((prepared_prices_df, prepared_phrases_df, prepared_news_df), use_text_model=True)
+ppp.predict(predict_date)
